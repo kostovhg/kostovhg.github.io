e3cc8b8671ce62ab8cc3233992f6545afc840f34
kostovhg <kostovhg@gmail.com>
2018-08-27 23:52:10 +0300

    Adding very important constant.js

---
+++ js/constants.js
@@ -0,0 +1,33 @@
+const [
+    layout,
+    nav, footer,
+    about, 
+    experience, experienceRecord,
+    education, edu, eduTab,
+    skills, skill,
+    projects, project,
+    login, register, contact
+] = [
+    './templates/layout.hbs',
+    './templates/common/nav.hbs', './templates/common/footer.hbs',
+    './templates/main/about.hbs', 
+    './templates/main/experience.hbs', './templates/partials/experienceRecord.hbs',
+    './templates/main/education.hbs', './templates/partials/edu.hbs', './templates/partials/eduTab.hbs',
+    './templates/main/skills.hbs', './templates/partials/skill.hbs',
+    './templates/main/projects.hbs', './templates/partials/project.hbs',
+    './templates/forms/login.hgs', './templates/forms/register.hbs', './templates/forms/contact.hbs'
+];
+
+const ROUTES = {
+    index: 'index.html',
+    about: '#/about',
+    experience: "#/experience",
+    skills: "#/skills",
+    education: "#/education",
+    projects: "#/projects",
+    contact: "#/contact",
+};
+
+function getLang() {
+    return "content/" + (sessionStorage.getItem('currentLanguage') || "en") + ".json";
+}
